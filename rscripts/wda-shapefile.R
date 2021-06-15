20edd1ee7998a229037ef3abc1bb9a04311a33a4
Shannon Carter <skcarter25@gmail.com>
2021-06-15 15:00:30 -0500

    started epxloratory plots script, color cat to shapefile

--- rscripts/wda-shapefile.R
+++ rscripts/wda-shapefile.R
@@ -3,7 +3,7 @@ library(tigris)
 library(here)
 library(sf)
 library(leaflet)
-library(highcharter)
+library(leaflet.extras)
 
 options(tigris_use_cache = TRUE, tigris_class = "sf")
 wgs84 <- st_crs("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs +towgs84=0,0,0")
@@ -11,11 +11,14 @@ wgs84 <- st_crs("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs +towgs84=0,0,0
 crosswalk <- read.csv(here::here("raw-data", "county_wda_crosswalk.csv"))
 
 counties <- tigris::counties(state = "Texas") %>% 
-  dplyr::select(county = NAME, geometry)
+  dplyr::select(county = NAME, geometry) %>% 
+  st_transform(crs = wgs84)
 
 wda_sf <- left_join(counties, crosswalk) %>% 
   group_by(wda) %>% 
   summarize(wda_number = wda_number[1]) %>% 
+  st_transform(crs = wgs84) %>% 
+  # manually assign a color category so we can have a map w 4 colors where no contiguous are the same color
   # https://docs.google.com/presentation/d/1Op61zVEh0M9EfREZEu0GQi8pMvI_i6wvDfN_z09Ckng/edit#slide=id.p
   mutate(color_category = case_when(wda_number %in% c(2, 25, 8, 12, 28, 22) ~ 1,
                               wda_number %in% c(1, 11, 4, 16, 19, 23) ~ 2,
@@ -24,14 +27,14 @@ wda_sf <- left_join(counties, crosswalk) %>%
 
 saveRDS(wda_sf, here::here("clean-data", "wda_shapefile.rds"))
 
-factpal <- colorFactor(palette = c("#66c2a5", "#fc8d62", "#8da0cb", "#e78ac3"), wda_sf$color_category)
-leaflet() %>%
-  addProviderTiles("CartoDB.Positron") %>%
-  setView(-99.9, 31.97, zoom = 5) %>%
+pal <- colorFactor(palette = c("#66c2a5", "#fc8d62", "#8da0cb", "#e78ac3"), wda_sf$color_category)
+leaflet() %>% #options = leafletOptions(zoomControl = FALSE, minZoom = 5, maxZoom = 5)) %>%
+  #addProviderTiles("CartoDB.Positron") %>%
+  setView(-99.9018, 30.9686, zoom = 5) %>% 
   addPolygons(stroke = F,
               fill = T,
               fillOpacity = 0.8,
-              fillColor = ~factpal(color_category),
+              fillColor = ~pal(color_category),
               data = wda_sf) %>%
   addPolygons(color = "black",
               stroke = T,
@@ -45,12 +48,9 @@ leaflet() %>%
               fill = T,
               fillOpacity = 0,
               label = wda_sf$wda,
-              data = wda_sf)
-
-
-hcmap("countries/us/us-tx-all", showInLegend = F) %>%
-  hc_title(text = "Texas") %>% 
-  hc_subtitle(text = "You can use the same functions to modify your map!") %>% 
-  hc_add_series(
-    data = wda_sf
-  )
+              data = wda_sf) %>% 
+  setMapWidgetStyle(list(background= "white")) %>% 
+  htmlwidgets::onRender("function(el, x) { 
+               map = this
+               map.dragging.disable();
+               }")
