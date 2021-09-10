5b8d64f32fa20c5ad84b0de268d23cb58f6c6790
davidmcclendon <davidmmcclendon@gmail.com>
2021-09-10 13:58:23 -0500

    update brookings data to aggregate to wda 24

--- rscripts/brookings-data.R
+++ rscripts/brookings-data.R
@@ -2,10 +2,18 @@ library(tidyverse)
 library(here)
 library(janitor)
 
+
+
+#Brookings data
 setwd(here::here("raw-data", "brookings"))
-files <- dir(path = here::here("raw-data", "brookings"), pattern = "*.csv")
-data <- tibble(File = files) %>%
-  mutate(Data = lapply(File, read_csv)) %>%
+
+#WDA28 to WDA24 crosswalk
+wda_crosswalk <- read_csv(here::here("raw-data","wda_brookings_crosswalk.csv"))
+
+#Import and clean data
+files <- dir(path = here::here("raw-data", "brookings"), pattern = "*.csv") 
+data <- tibble(File = files) %>% 
+  mutate(Data = lapply(File, read_csv)) %>% #This loads and binds all the files together
   unnest(Data) %>% 
   clean_names() %>% 
   rename(quality_index = quality_index_5_to_5,
@@ -17,8 +25,24 @@ data <- tibble(File = files) %>%
   mutate(wfb = sub(".* - ", "", file),
          wfb = gsub(".csv", "", wfb)) %>% 
   select(-file) %>% 
-  select(wda = wfb, occupational_code, occupation, share_of_local_jobs_percent, quality_index, demand_index, quality_and_demand_quadrant) 
+  select(wda = wfb, occupational_code, occupation, share_of_local_jobs_percent, quality_index, demand_index, quality_and_demand_quadrant) %>% 
+  left_join(., wda_crosswalk, by=c("wda" = "wda28"))
 
+#Aggregate up to our WDA 24
+data_wda24 <- data %>% 
+  group_by(wda.y, occupational_code, occupation) %>% 
+  summarize(share_of_local_jobs_percent = round(mean(share_of_local_jobs_percent, na.rm = T), 3),
+            quality_index = round(mean(quality_index, na.rm = T), 3),
+            demand_index = round(mean(demand_index, na.rm = T), 3)) %>% 
+  mutate(quality_score = case_when(quality_index >= 0 ~ "High quality",
+                                   quality_index < 0 ~ "Low quality"),
+         demand_score = case_when(demand_index >= 0 ~ "High demand",
+                                  demand_index < 0 ~ "Low demand")) %>% 
+  mutate(quality_and_demand_quadrant = paste0(quality_score, " - ", demand_score)) %>% 
+  rename(wda = wda.y) %>% 
+  select(-quality_score, -demand_score)
+
+#Aggregate up to Texas
 data_tx <- data %>% 
   group_by(occupational_code, occupation) %>% 
   summarize(share_of_local_jobs_percent = mean(share_of_local_jobs_percent, na.rm = T),
@@ -33,5 +57,6 @@ data_tx <- data %>%
   select(-quality_score, -demand_score)
     
   
-export <- rbind(data, data_tx) %>% 
+export <- rbind(data_wda24, data_tx) %>% 
+  filter(!is.na(occupation)) %>% 
   saveRDS(here::here("clean-data", "brookings-data.rds"))
